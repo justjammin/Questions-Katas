5c07f9bfb8822ebb2f1a10a0e2d530c56eb73abb
DekuBeats <55197244+justjammin@users.noreply.github.com>
2021-11-03 14:31:25 +0000

    Question 9

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-#Friday kata 10-29
-#Gooood morning and happy Friday! Here is today’s code kata that we’ll go over a bit later (9am in the best time zone there is), so you should have time to complete this.
-#It may be best to all put our answers in at codewars.com, so I’m linking the kata from there. For those that don’t want to do that, I’ll paste the test cases here as well.
-#https://www.codewars.com/kata/51b6249c4612257ac0000005/train/php
-#Description
-#Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don’t need to validate the form of the Roman numeral.
-#Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered “MCMXC” (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered “MMVIII” (2000 = MM, 8 = VIII). The Roman numeral for 1666, “MDCLXVI”, uses each letter in descending order.
-#Example:
-#solution(‘XXI’); // should return 21
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 
-I=1
-V=5
-X=10
-L=50
-C=100
-D=500
-M=1000
+#Ex. 1
+#Input: nums = [-4,-1,0,3,10]
+#Output: [0,1,9,16,100]
+
+#Ex. 2
+#Input: nums = [-7,-3,2,3,11]
+#Output: [4,9,9,49,121]
+
+nums = [anything]
+squares = []
+
+for num in nums:
+  squared = num**2
+  squares.append(squared)
+  continue
+squares.sort()
+print(squares)
+
 
